fef6f2b4103a719b31d0183a49de86f4e69e6380
Hexin Liu <32285422+Lhx94As@users.noreply.github.com>
2023-02-14 07:59:41 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 #$ -e /home/xzhan233/language_id/l.log
 #$ -o /home/xzhan233/language_id/d.txt
 
-predicting_file_folder='/home/xzhan233/language_id/pred'
-ground_truth_folder='/home/xzhan233/language_id/ref'
-result_output='/home/xzhan233/language_id'
+predicting_file_folder='/home/language_id/pred'
+ground_truth_folder='/home/language_id/ref'
+result_output='/home/language_id'
 
 python3 /home/xzhan233/language_id/scoring_diar.py --predicting_file ${predicting_file_folder} --ground_truth ${ground_truth_folder} --result_output ${result_output}
